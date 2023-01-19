51d9fc0cb9d1d0e93f4f56d7d41d16c23033d575
RazielHernandez <razo.puma.azteca@gmail.com>
2023-01-18 19:33:14 -0500

    GameObject class added

---
+++ MAPD724-W2023-ICE-1B/Core/GameObject.swift
@@ -0,0 +1,11 @@
+import GameplayKit
+import SpriteKit
+
+class GameObject : SKSpriteNode{
+    
+    var horizontalSpeed: CGFloat?
+    var verticalSpeed: CGFloat?
+    var width: CGFloat?
+    var height: CGFloat?
+    
+}
